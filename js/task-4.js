363766671a161f9f0eee498f1f5e6f1a29c46ebb
Iana Kamardina <kamardinkada@gmail.com>
2024-07-04 13:24:29 +0300

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+    const form = evt.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+};
