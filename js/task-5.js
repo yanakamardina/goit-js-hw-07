3024e8ff806468f55aa1ea9ee0f3043583bbd81c
Iana Kamardina <kamardinkada@gmail.com>
2024-07-04 21:45:05 +0300

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,16 @@
+const body = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+
+function ChangeColor() {
+  let finalColor = getRandomHexColor()
+  body.style.background = finalColor;
+  spanColor.textContent = finalColor;
+}
+
+btnChangeColor.addEventListener("click", ChangeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
